1eb7d73b017897fe4fe927eeca4c954f117c623c
Raytene Han <raytenehan@gmail.com>
2022-07-19 16:41:47 -0700

    added showGifs function to display gif on page

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ console.log("Let's get this party started!");
 
 
 $("form").on("submit", async function gifRequest(event) {
-    
+
     const inputVal = $("input").val();
-    
+
     event.preventDefault();
-    let gif = await axios.get("http://api.giphy.com/v1/gifs/search",{param: {q:`${inputVal}`, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" }});
+    let gif = await axios.get("http://api.giphy.com/v1/gifs/search",{params: {api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", q:`${inputVal}`}});
     console.log(gif.data);
+    showGifs(gif);
+
 
     //"
 
 });
+/** Accept a  */
+function showGifs(gif) {
+    let gifUrl = gif.data.data[0].images.original.url;
+    $(".gifs").append(`<img src="${gifUrl}">`);
+}
