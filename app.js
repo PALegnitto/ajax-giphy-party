26a45b0a88d009828a91df995806eec3871b53bb
Phill Legnitto <palegnitto@gmail.com>
2022-07-19 16:13:03 -0700

    create simple site and initial ajax request structure

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Let's get this party started!");
+
+
+$("form").on("submit", async function gifRequest(event) {
+    
+    const inputVal = $("input").val();
+    
+    event.preventDefault();
+    let gif = await axios.get("http://api.giphy.com/v1/gifs/search",{param: {q:`${inputVal}`, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym" }});
+    console.log(gif.data);
+
+    //"
+
+});
